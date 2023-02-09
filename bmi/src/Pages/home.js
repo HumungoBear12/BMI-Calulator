02eda0ec5689c37da33630f25707d00744d282cc
Andrew Makris <amakri386@west-mec.org>
2023-02-09 10:25:47 -0700

    Final

--- bmi/src/Pages/home.js
+++ bmi/src/Pages/home.js
@@ -5,7 +5,7 @@ function doMath() {
   var numOne = document.getElementById('num1').value;
   var numTwo = document.getElementById('num2').value;
   var theProduct = (parseInt(numOne) * num ) / (parseInt(numTwo) * parseInt(numTwo));     console.log (theProduct);
-  document.getElementById('ans').innerHTML = theProduct;
+  document.getElementById('ans').innerHTML = `Your Current BMI: <br/>${theProduct.toFixed(2)}`;
   }
 
 const home = () => {
@@ -18,17 +18,17 @@ const home = () => {
           <h2>BMI Calculator</h2>
           <hr/>
             <form>
-                Weight (In Pounds)<br/>
+                <p>Weight (In Pounds)</p>
                 <input id="num1" type="text" name="num1"/>
             <br/>
-                Height (In Inches)<br/>
+                <p>Height (In Inches)</p>
                 <input id="num2" type="text" name="num2"/>
                 <br></br>
               <input className='button' type="button" value="Calculate Your BMI!" onClick={doMath} /><p/>
-                Your Current BMI: <span id="ans"></span>
+                <h6><span id="ans"></span></h6>
             </form>
         </div>
-        <img src='https://www.fryemedctr.com/sites/frye/assets/uploads/BMI-Widget-Chart.png' alt=''/>
+        {/* <img src='https://www.fryemedctr.com/sites/frye/assets/uploads/BMI-Widget-Chart.png' alt=''/> */}
     </div>
   )
 }
